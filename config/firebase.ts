377619d10a8d1b0852f018fc0b1c1557c61e1f91
mblaul <mattblaul@gmail.com>
2019-12-26 19:54:05 -0500

    Adding firebase to project 🔥

---
+++ config/firebase.ts
@@ -0,0 +1,25 @@
+import { secrets } from './secrets'
+
+const { 
+  firebaseConfig: {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
+  },
+} = secrets;
+
+export const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
